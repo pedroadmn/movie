5d3a40e15293f7e8bbb6438129f00abb0aa25815
Pedro Adelino de Mello Neto <pedroadmn@gmail.com>
2020-05-13 15:40:12 -0300

    Adding Login Screen

---
+++ app/src/main/java/com/example/movie/ui/LoginActivity.java
@@ -0,0 +1,82 @@
+package com.example.movie.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.movie.R;
+import com.mobsandgeeks.saripaar.ValidationError;
+import com.mobsandgeeks.saripaar.Validator;
+import com.mobsandgeeks.saripaar.annotation.Email;
+import com.mobsandgeeks.saripaar.annotation.NotEmpty;
+import com.mobsandgeeks.saripaar.annotation.Password;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class LoginActivity extends AppCompatActivity implements Validator.ValidationListener {
+
+    @NotEmpty
+    @Email
+    @BindView(R.id.email)
+    EditText emailEditText;
+
+    @Password(min = 6, scheme = Password.Scheme.ALPHA_NUMERIC_MIXED_CASE_SYMBOLS)
+    @BindView(R.id.password)
+    EditText passwordEditText;
+
+    @BindView(R.id.login)
+    TextView login;
+
+    private Validator validator;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        ButterKnife.bind(this);
+
+        validator = new Validator(this);
+        validator.setValidationListener(this);
+
+        login = findViewById(R.id.login);
+
+        login.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                validator.validate();
+            }
+        });
+    }
+
+    @Override
+    public void onValidationSucceeded() {
+        Toast.makeText(this, "Login Succeeded!", Toast.LENGTH_SHORT).show();
+        Intent i = new Intent(getApplicationContext(), HomeActivity.class);
+        startActivity(i);
+    }
+
+    @Override
+    public void onValidationFailed(List<ValidationError> errors) {
+        for (ValidationError error : errors) {
+            View view = error.getView();
+            String message = error.getCollatedErrorMessage(this);
+
+            // Display error messages ;)
+            if (view instanceof EditText) {
+                ((EditText) view).setError(message);
+            } else {
+                Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+}
